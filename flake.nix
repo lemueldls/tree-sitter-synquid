62a32f72374107cb86b5df05aebb1a9f8feeeec7
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2024-06-25 03:48:55 -0400

    chore: file dump

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk/master";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk, poetry2nix }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          # buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+          # RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      }
+    );
+}
